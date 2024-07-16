a6f6699b7381a94dbd320a8a747cbb0aea6ea6ff
Victor <93835023+victor247k@users.noreply.github.com>
2024-07-16 03:16:40 +0300

    Added add, remove, category functions, and also added to be able to save your data

--- main.go
+++ main.go
@@ -6,16 +6,20 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"net/url"
+	"encoding/json"
+	"io/ioutil"
 )
 
 var cliName string = "RSSFeedReader"
+var linkSaveFile string = "./links.json"
 
 func printPrompt() {
 	fmt.Print(cliName, "> ")
 }
 
-func printUnknown(text string) {
-	fmt.Println(text, ": command not found")
+func printUnknown(cmd string, err string) {
+	fmt.Println(cmd, ":", err)
 }
 
 func displayHelp() {
@@ -28,12 +32,12 @@ func clearScreen() {
     cmd.Run()
 }
 
-func handleInvalidCmd(text string) {
-	defer printUnknown(text)
+func handleInvalidCmd(cmd string, err string) {
+	defer printUnknown(cmd, err)
 }
 
-func handleCmd(text string) {
-	handleInvalidCmd(text)
+func handleCmd(cmd string) {
+	handleInvalidCmd(cmd, "command not found")
 }
 
 func cleanInput(text string) string {
@@ -60,11 +64,146 @@ func formatInput(text string) []string {
 	return querry
 }
 
-func main() {
-	commands := map[string]interface{} {
-		".help": displayHelp,
-		".clear": clearScreen,
+func validateUrl(text string) bool {
+	_, err := url.ParseRequestURI(text)
+	if err != nil {
+		handleInvalidCmd(".add", "invalid url")
+		return false
 	}
+	return true
+}
+
+func addLink() {
+	size := len(querry)
+	if size != 2 && size != 3 {
+		handleInvalidCmd(".add", "invalid number of arguments")
+		return
+	}
+	var url string = querry[1]
+	var category string
+
+	if size == 2 {
+		category = "uncategorized"
+	} else {
+		category = querry[2]
+	}
+
+	if validateUrl(url) {
+		link_map[category] = append(link_map[category], url)
+	}
+}
+
+func removeLink() {
+	size := len(querry)
+	if size != 2 {
+		handleInvalidCmd(".remove", "invalid number of arguments")
+		return
+	}
+	var url_to_be_deleted string = querry[1]
+	for key, arr := range link_map {
+		newArr := arr[:0]
+		for _, url := range arr {
+			if !strings.EqualFold(url_to_be_deleted, url) {
+				newArr = append(newArr, url)
+			}
+		}
+		link_map[key] = newArr
+		if len(newArr) == 0 {
+			delete(link_map, key)
+		}
+	}
+}
+
+func changeCategory() {
+	size := len(querry)
+	if size != 3 {
+		handleInvalidCmd(".category", "invalid number of arguments")
+		return
+	}
+
+	category := querry[2]
+
+	newQuerry := querry[:2]
+	querry = newQuerry
+
+	removeLink()
+
+	querry = append(querry, category)
+	addLink()
+}
+
+func showLinks() {
+	size := len(querry)
+	if size != 1 && size != 2 {
+		handleInvalidCmd(".show", "invalid number of arguments")
+		return
+	}
+	fmt.Println()
+	if size == 2 {
+		category := querry[1]
+		fmt.Println()
+		fmt.Printf("%s:\n", category)
+		for idx, itm := range link_map[category] {
+			ch := ";"
+			if idx == len(link_map[category]) - 1 {
+				ch = "."
+			} 
+			fmt.Printf("    %d. %s %s\n", idx + 1, itm, ch)
+		}
+
+		fmt.Println()
+	} else {
+		for category, arr := range link_map {
+			fmt.Println()
+			fmt.Printf("%s:\n", category)
+			for idx, itm := range arr {
+				ch := ";"
+				if idx == len(arr) - 1 {
+					ch = "."
+				} 
+				fmt.Printf("    %d. %s %s\n", idx + 1, itm, ch)
+			}
+			fmt.Println()
+		}
+	}
+	fmt.Println()
+}
+
+func loadJsonFile() {
+	content, err := ioutil.ReadFile(linkSaveFile )
+	if err == nil {
+		json.Unmarshal(content, &link_map)
+	}
+}
+
+func saveLinksToFile() {
+	asJSON, err := json.MarshalIndent(link_map, "", "\t")
+	if err != nil {
+		fmt.Printf("Error marshalling data to JSON: %v\n", err)
+		return
+	}
+
+	// Write the JSON data to the specified file
+	writingErr := ioutil.WriteFile(linkSaveFile, asJSON, 0644)
+	if writingErr != nil {
+		fmt.Printf("Error writing to file: %v\n", writingErr)
+	}
+}
+
+var link_map = make(map[string][]string)
+var querry []string
+
+var commands = map[string]interface{} {
+	".help": displayHelp,
+	".clear": clearScreen,
+	".add": addLink,
+	".remove": removeLink,
+	".category": changeCategory,
+	".show": showLinks,
+}
+
+func main() {
+	loadJsonFile();
 
 	reader := bufio.NewScanner(os.Stdin)
 	printPrompt()
@@ -72,13 +211,14 @@ func main() {
 	for reader.Scan() {
 
 		input := cleanInput(reader.Text())
-		var querry []string = formatInput(input)
+		querry = formatInput(input)
 
 		var cmd string = querry[0]
 
 		if command, exists := commands[cmd]; exists {
 			command.(func()) ()
 		} else if strings.EqualFold(".exit", cmd) {
+			saveLinksToFile()
 			return
 		} else {
 			handleCmd(cmd)
@@ -86,4 +226,6 @@ func main() {
 		printPrompt()
 	}
 	fmt.Println()
+
+	saveLinksToFile()
 }
